00aa79f85a07d36891201ad59e08edbef9c227ea
NateRobinson <15951084423@163.com>
2016-01-08 17:24:56 +0800

    添加效果图

--- app/src/main/java/com/gu/task/MainActivity.java
+++ app/src/main/java/com/gu/task/MainActivity.java
@@ -1,9 +1,10 @@
 package com.gu.task;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.gu.task.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/gu/task/base/BaseActivity.java
@@ -0,0 +1,25 @@
+package com.gu.task.base;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+/**
+ * Created by Nate on 2016/1/8.
+ */
+public class BaseActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
